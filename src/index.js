f2583bd577926346eab6d5893371c7b2e653acd7
Tobias <githubst@gmail.com>
2018-06-26 23:18:05 +0200

    Adding Canvas

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ import { makeWarrior } from "./createCaracter.js";
  * Testing Button Create for later.
  */
 
-const createWarrior = document.querySelector('#warrior');
+const createWarrior = document.querySelector("#warrior");
 createWarrior.addEventListener("click", function() {
-    // Creating The Warrior //
-    makeWarrior();
+  // Creating The Warrior //
+  makeWarrior();
 });
+
+/**
+ * Get Canvas
+ */
+
+const canvas = document.querySelector("#gameWindow");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
