b0b8a30f1446b0e719f34ac91888c55a44767813
Tobias <githubst@gmail.com>
2018-08-16 18:21:25 +0200

    testing animation

--- src/gametest/canvas/canvasDraw.js
+++ src/gametest/canvas/canvasDraw.js
@@ -1,3 +1,15 @@
 import {createCanvas} from './createCanvas';
+import {SpriteAnimation} from '../movingSprite/movingSprite';
 
 createCanvas();
+
+/**
+ * basic animations
+ */
+/*
+const sprite = new SpriteAnimation();
+
+setInterval(() => {
+   sprite.drawSprite();
+}, 100);
+*/
